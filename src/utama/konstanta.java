f514c16a583f4193619dd086556d3bfc3913fc95
Dwiky Juniardi <zencraft468@gmail.com>
2024-12-16 19:15:39 +0700

    Add files via upload

--- src/utama/konstanta.java
+++ src/utama/konstanta.java
@@ -1,5 +1,9 @@
 package utama;
 
 public class konstanta {
-    public static String UID = null;
+    public static String UID = null; // UID Mahasiswa
+    public static String ID = null; // ID Admin
+    
+    public static String NIM = null;
+    public static boolean isEdit = false;
 }
