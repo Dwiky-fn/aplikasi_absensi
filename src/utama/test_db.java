2cdfe56568a866cd3f87929bcd7a74b2dada6f8a
Dwiky Juniardi <zencraft468@gmail.com>
2025-01-08 09:49:07 +0700

    Add files via upload

--- src/utama/test_db.java
+++ src/utama/test_db.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Random;
 
 public class test_db {
     private static final String URL = "jdbc:mysql://localhost:3306/aplikasi_absensi";
@@ -8,13 +9,8 @@ public class test_db {
     private static final String PASSWORD = "";
 
     public static void main(String[] args) {
-        try {
-            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            if (conn != null) {
-                System.out.println("Connected to the database.");
-            }
-        } catch (SQLException e) {
-            System.out.println("Connection failed: " + e.getMessage());
-        }
+        Random random = new Random();
+        int randomNumber = 10000000 + random.nextInt(90000000); // Angka 8 digit (10000000-99999999)
+        System.out.println("Angka random 8 digit: " + randomNumber);
     }
 }
